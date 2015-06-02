ca3810d2fa3e601a89433d70128d205dfaf94a80
Pushkareva Svetlana <nelikonya@gmail.com>
2015-06-02 21:29:12 +0600

    Добавлены задачки из лекции1

---
+++ 28.05_intro/z1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace z1
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            int x1 = 0;
+            int x2 = 0;
+
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Введите первое число:");
+                    x1 = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Введите второе число:");
+                    x2 = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Ответ:{0}",x1+x2);
+                }
+                catch
+                {
+                    Console.WriteLine("Введенные параметры должны быть числом");
+                }
+
+                Console.WriteLine("\nВыйти? y/n");
+                if ( Console.ReadLine() == "y")
+                    break;
+
+            }
+        }
+    }
+}
---
+++ 28.05_intro/z2.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace z2
+{
+    class Program
+    {
+
+        static int kol_bit = 64;
+
+        static void Main(string[] args)
+        {
+            int id;
+            int city;
+            int table;
+            int text;
+            int i = 0;
+            String var_string = "";
+            long num = 1267165676175383; //число
+            String num_txt = Convert.ToString(num, 2); //двоичное представление
+            int need = kol_bit - num_txt.Length;
+            while (num_txt.Length < 64)
+            {
+                num_txt = "0" + num_txt;
+            }
+
+            var_string = num_txt.Substring(0, 2);
+            id = Convert.ToInt32(var_string, 2);
+            var_string = num_txt.Substring(2, 15);
+            city = Convert.ToInt32(var_string, 2);
+            var_string = num_txt.Substring(17, 15);
+            table = Convert.ToInt32(var_string, 2);
+            var_string = num_txt.Substring(32, 32);
+            text = Convert.ToInt32(var_string, 2);
+
+
+            Console.WriteLine("id типа: " + id + "\nid города: " + city + "\nid таблицы: " + table + "\nid объекта: " + text);
+            Console.ReadKey();
+
+
+        }
+    }
+}
