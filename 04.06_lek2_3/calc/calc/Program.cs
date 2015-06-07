46434fc6937488bb060b42b0a139b835e5c7b525
Pushkareva Svetlana <nelikonya@gmail.com>
2015-06-07 20:19:22 +0600

    ДЗ для лекции2

---
+++ 04.06_lek2_3/calc/calc/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a;
+            double b;
+            char action=' ';
+            double answer=0;
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Введите левый операнд");
+                    a = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("Введите один из операторов: +,-,/,*,^,%");
+                    action = Convert.ToChar(Console.ReadLine());
+                    Console.WriteLine("Введите правый операнд");
+                    b = Convert.ToDouble(Console.ReadLine());
+
+                    answer = 0;
+
+                    switch (action)
+                    {
+                        case '+':
+                            answer = a + b;
+                            break;
+                        case '-':
+                            answer = a - b;
+                            break;
+                        case '*':
+                            answer = a*b;
+                            break;
+                        case '/':
+                            answer = a/b;
+                            break;
+                        case '^':
+                            answer = Math.Pow(a, b);
+                            break;
+                        case '%':
+                            answer = a%b;
+                            break;
+                        default: Console.WriteLine("Введенного оператора не существует. Повторите попытку\n"); continue;
+                    }
+                    Console.WriteLine("\nОтвет:{0}", answer);
+                    Console.WriteLine("Выйти? y/n");
+                    if (Console.ReadLine() == "y")
+                        break;
+                  
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Ой, произошла ошибка. Введите все данные заново ");
+                }
+                Console.WriteLine();
+            }
+
+
+        }
+    }
+}
---
+++ 04.06_lek2_3/matrix_min/matrix_min/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace matrix_min
+{
+    class Program
+    {
+        public static void printmatrix(int[][] matrix, int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write(matrix[i][j] + " ");
+
+                }
+                Console.WriteLine();
+            }
+        }
+        private static void Main(string[] args)
+        {
+            Random r = new Random();
+            int n;
+            Console.WriteLine("Введите число N для матрицы NxN");
+            n = Convert.ToInt32(Console.ReadLine());
+            int[][] matrix = new int[n][];
+
+            for (int i = 0; i < n; i++)
+            {
+                matrix[i] = new int[n];
+                for (int j = 0; j < n; j++)
+                    matrix[i][j] = r.Next(100);
+            }
+            Console.WriteLine("Исходная матрица:");
+            printmatrix(matrix,n);
+            Console.WriteLine("");
+            for (int i = 0; i < n; i++)
+            {
+            Console.WriteLine("Строка{0}:{1}",i+1,matrix[i].Min() );
+             }
+
+            return;
+            
+        }
+    }
+}
