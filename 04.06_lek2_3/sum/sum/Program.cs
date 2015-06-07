2b8999df92d104712ef7e6955d6be12a7b96a69f
Pushkareva Svetlana <nelikonya@gmail.com>
2015-06-07 20:29:28 +0600

    Добавила задание с суммой чисел

---
+++ 04.06_lek2_3/sum/sum/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sum
+{
+    class Program
+    {
+ 
+        static void Main(string[] args)
+        {
+            int N = 0;
+            int sum = 0;
+            Console.WriteLine("Введите количество чисел");
+            N=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите числа. Ввод нового числа осуществляется по нажатию на клавишу \"Enter\"");
+            while (N>0)
+            {
+                try
+                {
+                    sum += Convert.ToInt32(Console.ReadLine());
+                    ;
+                    N--;
+                }
+                catch (Exception e)
+                {
+                    continue;        
+                }
+
+            }
+            Console.WriteLine("Сумма чисел:"+sum);
+            Console.ReadKey();
+        }
+    }
+}
