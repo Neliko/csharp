3560b642aad4733c43a82cfd78b1bf3bbd3c4e93
Pushkareva Svetlana <nelikonya@gmail.com>
2015-06-09 00:25:27 +0600

    Лекция3. Количество слов в тексте

---
+++ 04.06_lek2_3/CountAllWords/countWords/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace СountAllWords
+{
+    class MyClass
+    {
+        static void Main(string[] args)
+        {
+           Console.WriteLine("Введите текст:");
+           String str = Console.ReadLine();
+           String[] newStrList = str.Split(' ');
+           Console.WriteLine("Количество слов в тексте:{0}", newStrList.Count());
+           return;
+        }
+    }
+}
