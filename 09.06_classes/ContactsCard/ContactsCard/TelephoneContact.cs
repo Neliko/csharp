4aeb1a3d3de3b94e3e5507f574a57493997175e4
Pushkareva Svetlana <nelikonya@gmail.com>
2015-06-15 00:39:55 +0600

    ДЗ по классам

---
+++ 09.06_classes/ContactsCard/ContactsCard/TelephoneContact.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactsCard
+{
+    class TelephoneContact : Contact
+    {
+        private string _telephoneZone;
+
+        public TelephoneContact()
+        {
+            _telephoneZone = "";
+        }
+     public TelephoneContact (string cname, string tz) : base(cname)
+     {
+         _telephoneZone = tz;
+     }
+
+        public override string ToString()
+        {
+            return base.ToString()+ string.Format("City code : {0}", _telephoneZone);
+        }
+    }
+}
---
+++ 09.06_classes/MainClass/MainClass/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Data;
+
+namespace MainClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Запуск из основного проекта. Данные переданы через конструктор");
+            Card c2 = new Card("ParameterMain", 99999, 9999999);
+            c2.GetData();
+            Console.ReadKey();
+
+        }
+    }
+}
