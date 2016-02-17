c3ddb574a12cbea12fe33f21d0be6babc1b52f87
Pushkareva Svetlana <s.pushkareva@2gis.ru>
2016-02-18 00:22:59 +0600

    SOLID: Очередная попытка прийти к успеху

--- SOLID/BL/AddingValidationService.cs
+++ SOLID/BL/AddingValidationService.cs
@@ -7,36 +7,19 @@ using HomeWork.Validation;
 
 namespace HomeWork.BL
 {
-    class AddingValidationService<TEntity> where TEntity : IEntity
+    class ValidationAndAddingService<TEntity> where TEntity : IEntity
     {
-        private ILogger logger;
-        private readonly IDictionary<string, object> ValidatorGetterDictionary;
+        private IValidator<TEntity> validator;
 
-        public AddingValidationService(ILogger logger)
+        public ValidationAndAddingService(IValidator<TEntity> validator)
         {
-            this.logger = logger;
-            ValidatorGetterDictionary = new Dictionary<string, object>
-            {
-                {
-                   "Email",  new EmailValidator(logger)
-                },
-                {
-                   "Phone",  new PhoneValidator(logger) 
-                }
-            };
+            this.validator = validator;
         }
 
         public void ValidateAndAddEntity(IRepository<TEntity> repository, TEntity entity)
         {
-            var entityType = entity.GetType().Name;
-
-            if (ValidatorGetterDictionary.ContainsKey(entityType))
-            {
-                var entityValidator = ValidatorGetterDictionary[entityType];
-                
-                if(((IValidator<TEntity>)entityValidator).IsValid(entity))
-                    repository.Add(entity); 
-            }  
+            if (validator.IsValid(entity))
+                repository.Add(entity);
         }
     }
 }
--- SOLID/Program.cs
+++ SOLID/Program.cs
@@ -1,33 +1,45 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Xml;
 using HomeWork.BL;
 using HomeWork.Data;
 using HomeWork.Infrastructure;
 using HomeWork.Model;
+using HomeWork.Validation;
 
 namespace HomeWork
 {
     class Program
     {
+        const string TxtName = "logfile.txt";
+        private static readonly ILogger FileLogger = new FileLogger(TxtName);
+
+        //IDictionary<string, object> ValidatorGetterDictionary = new Dictionary<string, object>
+        //    {
+        //        {
+        //           "Email",  new EmailValidator(FileLogger)
+        //        },
+        //        {
+        //           "Phone",  new PhoneValidator(FileLogger) 
+        //        }
+        //    };
+
         static void Main(string[] args)
         {
-            var user = new User{ Id = 1, Name = "Name" };
+            var user = new User { Id = 1, Name = "Name" };
 
-            var phone = new Phone {Id = 1, PhoneCode = null, Value = "123124" };
+            var phone = new Phone { Id = 1, PhoneCode = "123", Value = "123124" };
             var email = new Email { Id = 2, Value = "mail@2gis.ru" };
 
-            const string txtName = "logfile.txt";
-            var fileLogger = new FileLogger(txtName);
-            var exceptionHandler = new ExceptionHandler(fileLogger);
+            var exceptionHandler = new ExceptionHandler(FileLogger);
 
             var userRepository = GetRepository<User>(exceptionHandler);
             userRepository.Add(user);
 
             var contactRepository = GetRepository<Contact>(exceptionHandler);
 
-            var testService = new AddingValidationService<Contact>(fileLogger);
-            testService.ValidateAndAddEntity(contactRepository, email);
-            testService.ValidateAndAddEntity(contactRepository, phone);
+            new ValidationAndAddingService<Contact>(new EmailValidator(FileLogger)).ValidateAndAddEntity(contactRepository, email);
+            new ValidationAndAddingService<Contact>(new PhoneValidator(FileLogger)).ValidateAndAddEntity(contactRepository, phone);
 
             Console.WriteLine(contactRepository.GetById(1));
 
@@ -35,6 +47,12 @@ namespace HomeWork
 
             Console.ReadKey();
         }
+
+        //public IValidator<TEntity> GetValidator<TEntity>(TEntity entity) where TEntity : IEntity
+        //{
+        //    return (IValidator<TEntity>)ValidatorGetterDictionary[entity.GetType().Name];
+
+        //}
         private static IRepository<TEntity> GetRepository<TEntity>(IExceptionHandler exceptionHandler)
                where TEntity : class, IEntity, new()
         {
